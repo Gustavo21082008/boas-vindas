325509a657e81973413aa0903c5ad4f10105242b
Gustavo21082008 <shark06874@sharkcoders.pt>
2024-10-26 11:43:57 +0100

    Add files via upload

---
+++ 2.py
@@ -0,0 +1,10 @@
+a = int(input("primeiro número:"))
+operacao = input('que operação vamos fazer?')
+b = int(input('segundo numero:'))
+if operacao == 'adição':
+    print(a+b)
+if operacao == 'subtração':
+    print(a-b)
+if operacao == 'divisão':
+    print(a / b)
+
