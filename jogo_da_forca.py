88b7821e8ae687a9e01df002c5e44f3de82a3906
Gustavo21082008 <shark06874@sharkcoders.pt>
2025-02-08 13:01:43 +0000

    Add files via upload
    updated version of the project

--- jogo_da_forca.py
+++ jogo_da_forca.py
@@ -1,20 +1,31 @@
 import random
 
+
 def escolha_palavra():
-    palavra=["montanha","floresta","casa","carro"]
+    palavra = ["montanha", "floresta", "casa", "carro","python"]
     return random.choice(palavra)
+
+
+def palavra_secreta(palavra, letras_adivinhadas):
+    palavra_adivinhada = ""
+    for X in palavra:
+        if letras_adivinhadas == True:
+         palavra +=
+
+
 
 def jogar():
     palavra = escolha_palavra()
-    print(palavra)
     tentativas = 6
     letras_adivinhadas = []
+
     # criar lista de letras adivinhadas
-    while tentativas>0:
+    while tentativas > 0:
+        palavra_secreta(palavra,letras_adivinhadas)
         letra = input("escolhe uma letra:  ").lower()
         #verificar se a letra  já foi adivinhada
         if letra in letras_adivinhadas:
-            print("letra usada tente outa")
+            print("letra usada tente outra")
             continue
         letras_adivinhadas.append(letra)
         if letra in palavra:
@@ -24,7 +35,17 @@ def jogar():
             tentativas -= 1
         if tentativas == 0:
             print("perdeste")
-        palavras_adivinhada = True
-        for i in palavra:
+        palavra_adivinhada = True
+        for letra in palavra:
+            if letra not in letras_adivinhadas:
+                palavra_adivinhada = False
+            break
+
+        if palavra_adivinhada:
+            print("parabéns ganhaste o jogo")
+            break
+        else:
+            print("perdeste a palavra secreta era", palavra)
+
 
 jogar()
