a5b2df0d194dc4f2f064de8f9b14f84017658106
Gustavo21082008 <shark06874@sharkcoders.pt>
2025-03-01 13:04:54 +0000

    Add files via upload

--- jogo do galo.py
+++ jogo do galo.py
@@ -1,9 +1,23 @@
 import tkinter as tk
-root = tk.Tk()
-#variável
-root.title("Jogo do Velha")
+def clica_botao(index):
+ global Jogador_Atual, board,buttons
 
-def Velha():
-    for i in range(9) button1 = tk.Button(root, text="yo")
-    button1.grid(row=i//3,collum=i%0)
+ if board[index] == "":
+  board[index] = Jogador_Atual
+  buttons[index].config(text=Jogador_Atual)
+   Jogador_Atual = "O" if Jogador_Atual == "X" else "X"
+
+root = tk.Tk()
+# variável
+root.title("Jogo do Velha")
+Jogador_Atual = "X" #define o jogador inicial com "X"
+board = [""] * 9
+buttons = []
+
+for i in range(9):
+   button = tk.Button(root, text='',font=('normal',40),width=5, height=2,command=lambda i=i: clica_botao(i))
+   button.grid(row=i//3, column=i%3)
+   buttons.append(button)
+
+
 root.mainloop()
